3f57b67997b5c6f58340dee7f6502f012cd6e88e
Bruna Fraga <brunafraga90@gmail.com>
2026-06-06 18:29:29 -0300

    feat: implementado interfaces e types respectivos a entidade Pokemon

---
+++ src/models/Pokemon.ts
@@ -0,0 +1,36 @@
+export interface Tipos {
+    tipo: {
+        name: string;
+    };
+};
+
+export interface Stats {
+    base_stat: number;
+    stat: {
+        name: string;
+    };
+};
+
+export interface PokemonApiResponse {
+    readonly id: number;
+    name: string;
+    height: number;
+    weight: number;
+    types: Tipos[];
+    stats: Stats[];
+};
+
+export interface PokemonResumo {
+    readonly id: number;
+    nome: string;
+    tipos: string[];
+    altura: number;
+    peso: number;
+};
+
+export interface LocalPokemon extends PokemonResumo {
+    estatistica: string[];
+    adicionadoEm: string;
+};
+
+export type BuscaPokemon = number | string;
