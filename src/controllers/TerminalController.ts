4d6cee61c1a91ba896f6025dfcb82a2751d1fef1
Bruna Fraga <brunafraga90@gmail.com>
2026-06-07 19:33:49 -0300

    style: melhora formatação visual no terminal

--- src/controllers/TerminalController.ts
+++ src/controllers/TerminalController.ts
@@ -12,11 +12,11 @@ export class TerminalController {
 
     public async start(): Promise<void> {
 
-        console.log('=========================================\n');
-        console.log('    INICIANDO POKÉMON TYPESCRIPT LITE    \n');
-        console.log('=========================================\n');
-        console.log('##  Bem vinde ao Catálogo de Pokémon  ##\n');
-        console.log('----------------------------------------\n');
+        console.log('==================================================================================\n');
+        console.log('                       INICIANDO POKÉMON TYPESCRIPT LITE                          \n');
+        console.log('==================================================================================\n');
+        console.log('#####                  Bem vinde ao Catálogo de Pokémon                      #####\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         // Teste de Busca Válida por nome e Adicionar pokemon
         console.log('Buscando Pokemon por nome Pikachu e adicionando ao catálogo');
@@ -25,7 +25,7 @@ export class TerminalController {
         if (pikachu !== null) {
             await this.boxService.adicionar(pikachu);
         };
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         // Teste de Busca Válida por id
         console.log('Buscando Pokemon pelo id 7 (Squirtle) e exibindo sem adicionar ao catálogo');
@@ -33,7 +33,7 @@ export class TerminalController {
         if (squirtle !== null) {
             console.log(textPokemon(squirtle));
         };
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         // Teste adicionar pokemon buscado por id
         console.log('Buscando Pokemon pelo id 17 (Pidgeotto) e adicionando ao catálogo');
@@ -41,7 +41,7 @@ export class TerminalController {
         if (Pidgeotto !== null) {
             await this.boxService.adicionar(Pidgeotto);
         };
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         // Adicionando mais pokemóns por id para aumentar o catálogo
         console.log('Adicionando mais pokémons pelo id...')
@@ -50,25 +50,25 @@ export class TerminalController {
         if (pokemon90 !== null) {
             await this.boxService.adicionar(pokemon90);
         }
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         const pokemon1 = await this.apiService.buscarPokemon(1);
         if (pokemon1 !== null) {
             await this.boxService.adicionar(pokemon1);
         }
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         const pokemon23 = await this.apiService.buscarPokemon(23);
         if (pokemon23 !== null) {
             await this.boxService.adicionar(pokemon23);
         }
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         const pokemon65 = await this.apiService.buscarPokemon(65);
         if (pokemon65 !== null) {
             await this.boxService.adicionar(pokemon65);
         }
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
         
         // Teste tratamento de erro para caso de adição com duplicidade
         console.log('Tentando adicionar pokémon duplicado... Pikachu');
@@ -77,38 +77,38 @@ export class TerminalController {
         if (pikachuDuplicado !== null) {
             await this.boxService.adicionar(pikachuDuplicado);
         }
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         // Teste tratamento de erro pokémon inexistente
         console.log('Tentando buscar pokémon inexistente...');
         const pokemonInexistente = await this.apiService.buscarPokemon("pokemónInexistente");
 
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         // Teste Listar Catálogo
         console.log('Listando catálogo...');
         await this.boxService.listar();
 
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         // Teste Remover pokemon por id (pokemon não adicionado ao catálogo)
         console.log('Removendo Pokemon por id 45 que não consta em catálogo...');
         await this.boxService.remover(45);
 
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         // Teste Remover pokemon por id (pokemon já adicionado ao catálogo)
         console.log('Removendo Pokemon por id 17 (Pidgeotto) do catálogo...');
         await this.boxService.remover(17);
 
-        console.log('----------------------------------------\n');
+        console.log('----------------------------------------------------------------------------------\n');
 
         // Teste Listar Catálogo após remoção de pokémon
         console.log('Listando catálogo após remoção de pokémon...');
         await this.boxService.listar();
 
-        console.log('=========================================\n');
-        console.log('    POKÉMON TYPESCRIPT LITE ENCERRADO    \n');
-        console.log('=========================================\n');
+        console.log('==================================================================================\n');
+        console.log('                       POKÉMON TYPESCRIPT LITE ENCERRADO                          \n');
+        console.log('==================================================================================\n');
     };
 };
